1baba65260d601b4f78a7a745751cec4c899f25c
Bogdan <bogdanparshencev@gmail.com>
2016-09-14 17:44:39 +0300

    Fix styl

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
 
   def index
+    @tests = Test.all
   end
 
   def edit
@@ -25,6 +26,12 @@ class TestsController < ApplicationController
     else
       redirect_to :back , flash: { error: @test.errors.full_messages.first }
     end
+  end
+
+  def destroy
+    test = Test.find_by_id(params[:id])
+    test.destroy
+    redirect_to tests_path, flash: { notice: "Test Deleted Successfully" }
   end
 
   def test_params
