9d152c0f873e3880cca78e30c1b4666dbf5ed979
Bogdan <bogdanparshencev@gmail.com>
2016-09-19 11:39:23 +0300

    Change form builder for create test

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,3 +1,10 @@
+
+$(document).ready(function() {
+    $('input[type="radio"]').click(function() {
+        show_time_section();
+    });
+});
+
 function show_time_section(){
     $("#set_time_for_test").show();
     if($('#test_time_limit_false')[0].checked){
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,5 +1,5 @@
 module TestsHelper
   def show_time_duration(test)
-    test.time_for_test.present? ? test.time_for_test.to_s + ' min' : '0 min'
+    test.time_for_test > 0 ? test.time_for_test.to_s + ' min' : 'N/A'
   end
 end
