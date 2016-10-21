848124c9529a32e282c565a6b0e03002f85d719f
Bogdan <bogdanparshencev@gmail.com>
2016-10-21 13:35:00 +0300

    Delete fields

--- db/migrate/20161021102459_refactoring_models.rb
+++ db/migrate/20161021102459_refactoring_models.rb
@@ -1,4 +1,12 @@
 class RefactoringModels < ActiveRecord::Migration[5.0]
   def change
+    add_column :response_fields, :user_content, :text
+    add_column :response_options, :user_selected, :boolean
+
+    remove_column :response_questions, :question_type, :integer
+    remove_column :response_questions, :score, :integer
+
+    remove_column :test_questions, :question_type, :integer
+    remove_column :test_questions, :score, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020165349) do
+ActiveRecord::Schema.define(version: 20161021102459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 20161020165349) do
     t.datetime "updated_at",     null: false
     t.text     "description"
     t.integer  "required_score"
-    t.integer  "score_units"
     t.string   "uuid"
+    t.integer  "score_units"
     t.integer  "order_index"
     t.index ["response_id"], name: "index_response_sections_on_response_id", using: :btree
   end
