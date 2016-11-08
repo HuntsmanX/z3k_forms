ec57cc1d61f5c65bfa7c40da361d086c0aa9bed1
Bogdan <bogdanparshencev@gmail.com>
2016-11-08 14:44:59 +0200

    Add fields type for checker

--- app/services/response_section_checker.rb
+++ app/services/response_section_checker.rb
@@ -1,9 +1,11 @@
 class ResponseSectionChecker
 
   methods_names_for_select = [:check_checkboxes, :check_dropdown,
-                              :check_sequence, :check_radio_buttons]
+                              :check_sequence, :check_radio_buttons,
+                              :check_inline_dropdown]
 
-  method_names_for_text = [:check_text_input, :check_text_area]
+  method_names_for_text = [:check_text_input, :check_text_area,
+                           :check_inline_text_input]
 
   def self.can_visit_next_section?(response_section)
     user_score = []
